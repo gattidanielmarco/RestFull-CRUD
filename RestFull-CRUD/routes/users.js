5da173958a942a8a97d56fe9170f81c2b2acc88f
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-01-02 13:50:43 +0000

    sudy

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -5,7 +5,7 @@ const config = {
   user: '4DD_13',  //Vostro user name
   password: 'xxx123##', //Vostra password
   server: "213.140.22.237\\sqlexpress",  //Stringa di connessione
-  database: 'wtSchool', //(Nome del DB)
+  database: '4DD_13', //(Nome del DB)
 }
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -14,7 +14,7 @@ router.get('/', function(req, res, next) {
     
     // Query
     let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from school.Person', (err, result) => {
+    sqlRequest.query('select * from persona', (err, result) => {
         if (err) console.log(err); // ... error checks
         res.send(result);  //Invio il risultato
     });
@@ -26,7 +26,7 @@ router.get('/search/:name', function(req, res, next) {
     if(err) console.log(err);
     // Query
     let sqlRequest = new sql.Request();
-    sqlRequest.query(`select * from [School].[Person] where FirstName = '${req.params.name}'`, (err, result) => {
+    sqlRequest.query(`select * from persona where FirstName = '${req.params.name}'`, (err, result) => {
         // ... error checks
         if (err) console.log(err);
 
