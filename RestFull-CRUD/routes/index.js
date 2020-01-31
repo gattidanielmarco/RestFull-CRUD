4a4e59fb004b03474e06c6b4cd61cf2e88ddb998
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-01-31 21:12:01 +0000

    punto 6.3.1 non finito

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
+var express = require('express');
+var router = express.Router();
+const sql = require('mssql');
 
+const config = {
+  user: '4DD_13',  //Vostro user name
+  password: 'xxx123##', //Vostra password
+  server: "213.140.22.237",  //Stringa di connessione
+  database: '4DD_13', //(Nome del DB)
+}
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let unit = req.body; 
+  unit = "select * from dbo.[cr-unit-attributes]";
+ 
+  
+  res.render('index', { title: 'Express', unit });
 });
 
 module.exports = router;
