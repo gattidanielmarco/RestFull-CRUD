9b97f2ceca153b690413f17fe5cb0e399c4356ca
gattidanielmarco <gatti.daniel.marco@iisgalvanimi.edu.it>
2020-02-18 11:51:59 +0000

    penultimo punto

--- RestFull-CRUD/routes/index.js
+++ RestFull-CRUD/routes/index.js
@@ -6,68 +6,72 @@ var router = express.Router();
 const sql = require('mssql');
 
 const config = {
-  user: '4DD_13',  //Vostro user name
-  password: 'xxx123##', //Vostra password
-  server: "213.140.22.237",  //Stringa di connessione
-  database: '4DD_13', //(Nome del DB)
+    user: '4DD_13',  //Vostro user name
+    password: 'xxx123##', //Vostra password
+    server: "213.140.22.237",  //Stringa di connessione
+    database: '4DD_13', //(Nome del DB)
 }
 let executeQuery = function (res, query, next, pagina, sendData) {
-  sql.connect(config, function (err) {
-    if (err) { //Display error page
-      console.log("Error while connecting database :- " + err);
-      res.status(500).json({success: false, message:'Error while connecting database', error:err});
-      return;
-    }
-    var request = new sql.Request(); // create Request object
-    request.query(query, function (err, result) { //Display error page
-      if (err) {
-        console.log("Error while querying database :- " + err);
-        res.status(500).json({success: false, message:'Error while querying database', error:err});
-        sql.close();
-        return;
-      }
-    /*  res.render('unita',{unita : result.recordset}); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
-      res.render('unit',{unita : result.recordset});
-    */
-      if (sendData) renderizza(pagina,res, result.recordset)
-      sql.close();
-    });
+    sql.connect(config, function (err) {
+        if (err) { //Display error page
+            console.log("Error while connecting database :- " + err);
+            res.status(500).json({ success: false, message: 'Error while connecting database', error: err });
+            return;
+        }
+        var request = new sql.Request(); // create Request object
+        request.query(query, function (err, result) { //Display error page
+            if (err) {
+                console.log("Error while querying database :- " + err);
+                res.status(500).json({ success: false, message: 'Error while querying database', error: err });
+                sql.close();
+                return;
+            }
+            /*  res.render('unita',{unita : result.recordset}); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
+              res.render('unit',{unita : result.recordset});
+            */
+            if (sendData) {
+                console.log("enter");
+                renderizza(pagina, res, result.recordset);
+            }
+            sql.close();
+        });
 
-  });
+    });
 }
 
-renderizza = function(pagina, res, dati)
-{
-    res.render(pagina,{unita : dati})
+renderizza = function (pagina, res, dati) {
+    res.render(pagina, { unita: dati })
 }
 
 /* GET home page. */
-router.get('/index', function(req, res, next) {
-  res.render('index');
+router.get('/index', function (req, res, next) {
+    res.render('index');
 });
-router.get('/', function(req, res, next) {
-  let unita = "select * from dbo.[cr-unit-attributes]";
-  executeQuery(res, unita, next, "unita", true);
+router.get('/', function (req, res, next) {
+    let unita = "select * from dbo.[cr-unit-attributes]";
+    executeQuery(res, unita, next, "unita", true);
 });
-router.get('/unit/:nome', function(req, res, next) {
+router.get('/unit/:nome', function (req, res, next) {
 
     let unita = `select * from dbo.[cr-unit-attributes] WHERE Unit = '${req.params.nome}'`;
     executeQuery(res, unita, next, "unit", true);
-  
+
 });
 
 router.post('/', function (req, res, next) {
-  // Add a new Unit  
-  console.log("ciao");
-  let unit = req.body;
-  if (!unit) {  //Qui dovremmo testare tutti i campi della richiesta
-    res.status(500).json({success: false, message:'Error while connecting database', error:err});
-    return;
-  }
-  let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
+    // Add a new Unit  
+    console.log("ciao");
+    let unit = req.body;
+    if (!unit) {  //Qui dovremmo testare tutti i campi della richiesta
+        res.status(500).json({ success: false, message: 'Error while connecting database', error: err });
+        return;
+    }
+    let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
                      VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
-  executeQuery(res, unita, next, "unit", false);
-  res.send("unit", unita: [unit])
+    executeQuery(res, sqlInsert, next, "inserimento", false);
+    vett = [];
+    vett.push(unit);
+    res.render("inserimento", { unita: vett })
 });
 
 
